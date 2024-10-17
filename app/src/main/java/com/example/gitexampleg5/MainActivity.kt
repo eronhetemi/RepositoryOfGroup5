1104c89564a2372efb316b264c4d603aa4cdfdae
eronhetemi <ehetemi23@gmail.com>
2024-10-17 13:40:08 +0200

    first on boarding

---
+++ app/src/main/java/com/example/gitexampleg5/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gitexampleg5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Those are some of the first changes
+    }
+}
